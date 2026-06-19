b4479067f653e69c4534f9b003b56b798f25e781
Charlie Tanfoll <94414525+charlietanfoll@users.noreply.github.com>
2026-06-19 19:43:45 -0300

    Add TreeAVL class with basic methods

---
+++ src/TreeAVL.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+class TreeAVL {
+    public:
+    void insert(int isbn, int ano_publicacao, std::string titulo, std::string autor) {
+        // Implement the insert logic for AVL tree
+    }
+    void remove(int isbn) {
+        // Implement the remove logic for AVL tree
+    }
+    void search(int isbn) {
+        // Implement the search logic for AVL tree
+    }
+
+    private:
+    
+
+};
