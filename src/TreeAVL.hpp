284f04f6bf1696ff8725b8817ff5c991fd037625
Charlie Tanfoll <94414525+charlietanfoll@users.noreply.github.com>
2026-06-21 06:01:25 +0000

    Implementação da lógica de inserção na classe TreeAVL e adição do campo balance na estrutura Node

--- src/TreeAVL.hpp
+++ src/TreeAVL.hpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <string>
+#include <memory>
+#include "NodeTree.hpp"
+
+using namespace std;
 
 class TreeAVL {
     public:
     void insert(int isbn, int ano_publicacao, std::string titulo, std::string autor) {
-        // Implement the insert logic for AVL tree
+        Node novonode {isbn, ano_publicacao, titulo, autor, 0, nullptr, nullptr};
+        Node *current = root.get();
+        if (root == nullptr) {
+            root = make_unique<Node>(novonode);
+        } else {
+            while (true) {
+                if (isbn < current->isbn) {
+                    if (current->left == nullptr) {
+                        current->left = make_unique<Node>(novonode);
+                        current->balance--;
+                        break;
+                    } else {
+                        current = current->left.get();
+                    }
+                } 
+            }
+        }
     }
     void remove(int isbn) {
         // Implement the remove logic for AVL tree
@@ -15,5 +35,7 @@ class TreeAVL {
 
     private:
     
+    unique_ptr<Node> root;
+
 
 };
